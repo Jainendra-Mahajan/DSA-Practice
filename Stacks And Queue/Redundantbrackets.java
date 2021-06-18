9fafd9a086059cac84f1c3e0f304e07af0a7d02b
Jainendra Mahajan <jainendra.mahajan456@gmail.com>
2021-06-18 14:19:05 +0530

    Check for Redundant brackets

---
+++ Stacks And Queue/Redundantbrackets.java
@@ -0,0 +1,37 @@
+// This runs as if we see stack top has '(' means there are no 
+//oprands in b/w them so return true
+
+// TC & SC - O(N)
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    
+	public static void main (String[] args) {
+	Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+    
+    Stack<Character>  st = new Stack<>();
+    for(int i = 0;  i <  str.length(); i++){
+        char ch = str.charAt(i);
+        
+        if(ch == ')'){
+            if(st.peek() == '('){
+                System.out.println(true);
+                return;
+            }
+            else {
+                while(st.peek() != '('){
+                    st.pop();
+                }
+                st.pop();
+            }
+        }
+        else 
+            st.push(ch);
+        
+    }
+    System.out.println(false);
+	    
+	}
+}
