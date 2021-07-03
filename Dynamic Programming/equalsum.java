29ea5b997c893dc7e6d703dd95c5366a23cb420b
Jainendra Mahajan <jainendra.mahajan456@gmail.com>
2021-07-03 14:45:41 +0530

    Equal sum partition DP

---
+++ Dynamic Programming/equalsum.java
@@ -0,0 +1,44 @@
+// User function Template for Java
+
+class equalSum{
+    static int equalPartition(int n, int arr[])
+    {
+      int sum = 0;
+      
+      for(int i = 0 ; i < n; i++){
+          sum += arr[i];
+      }
+      
+      if(sum % 2 != 0) return 0;
+      
+      if(subsetSum(arr , n , sum / 2)) return 1;
+    
+     return 0;
+    }
+    
+   static boolean subsetSum(int[] arr ,  int n , int sum){
+        boolean[][] dp = new boolean[n + 1][sum + 1];
+        
+        for(int i = 0; i < n + 1; i++){
+            for(int j = 0; j < sum + 1; j++){
+                if(i == 0) 
+                dp[i][j] = false;
+                
+                if(j == 0)
+                dp[i][j] = true;
+            }
+        }
+        
+        for(int i = 1; i < n + 1; i++){
+            for(int j = 1; j < sum + 1; j++){
+                if(arr[i - 1] <= j){
+                    dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i-1][j];
+                }
+                else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+}
